2bd5340fdbfd0f0215e3fb70f04f0a77612ffb42
Sunny Zhang <shuningzhang@126.com>
2019-04-13 18:26:28 +0800

    multthread: add some commit
    Signed-off-by: Sunny Zhang <shuningzhang@126.com>

--- multthread/spinlock/multhread.c
+++ multthread/spinlock/multhread.c
@@ -12,15 +12,19 @@
 #include <linux/spinlock.h>
 
 #define BUF_SIZE 1024
-struct task_struct *main_task;
-struct task_struct *client_task;
-spinlock_t lock;
-struct task_data thread_data;
-
 
 struct task_data {
 	int progress;
 };
+
+struct task_struct *main_task;
+struct task_struct *client_task;
+
+/* 在这里定义自旋锁及要保护的数据，这里只是为了说明用法
+ * 实际生产中不会这么用，因为对于一个简单数据，可以通过
+ * 原子变量实现。 */
+spinlock_t lock;
+struct task_data thread_data;
 
 static inline void sleep(unsigned sec)
 {
@@ -28,14 +32,19 @@ static inline void sleep(unsigned sec)
 	schedule_timeout(sec * HZ);
 }
 
+/* 这个是2个线程中的一个，只是为了说明自旋锁的用法。  */
 static int multhread_server(void *data)
 {
 	struct task_data *cur = (struct task_data *)data;
 	while (!kthread_should_stop()) {
 		printk(KERN_NOTICE "server thread run begin %d\n", cur->progress);
 		sleep(1);
+	
+		/* 自旋锁加锁 */
 		spin_lock(&lock);
+		/* 临界区，也就是被保护的数据的操作 */
 		cur->progress ++;
+		/* 自旋锁解锁 */
 		spin_unlock(&lock);
 		printk(KERN_NOTICE "server thread run after %d\n", cur->progress);
 
